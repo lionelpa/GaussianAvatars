058fa806e46cd096c0a18c7ee526ff416578166f
Lionel Pereira Azevedo <lionel-azevedo@gmx.de>
2024-09-16 13:14:05 +0200

    Prettify project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# prettyfy util method
+print_decorated_message() {
+    echo
+    echo "============================================"
+    echo "$1"
+    echo "============================================"
+    echo
+}
+
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
@@ -11,33 +21,33 @@ if ! command -v conda &> /dev/null; then
 fi
 
 # Step 1: Create the conda environment with Python 3.10
-echo "Creating conda environment 'avatars'..."
+print_decorated_message "Creating conda environment 'avatars'..."
 conda create --name avatars -y python=3.10
 
 # Step 2: Activate the conda environment
-echo "Activating the environment..."
+print_decorated_message "Activating the environment..."
 source $(conda info --base)/etc/profile.d/conda.sh  # Ensure conda is recognized in scripts
 conda activate avatars
 
 # Step 3: Install the CUDA toolkit and Ninja
-echo "Installing CUDA toolkit and Ninja..."
+print_decorated_message "Installing CUDA toolkit and Ninja..."
 conda install -c "nvidia/label/cuda-11.7.1" cuda-toolkit ninja -y
 
 # Step 4: Create symbolic link to avoid linker error
-echo "Creating symlink for lib64..."
+print_decorated_message "Creating symlink for lib64..."
 ln -s "$CONDA_PREFIX/lib" "$CONDA_PREFIX/lib64"
 
 # Step 5: Set CUDA_HOME environment variable
-echo "Setting CUDA_HOME environment variable..."
+print_decorated_message "Setting CUDA_HOME environment variable..."
 conda env config vars set CUDA_HOME=$CONDA_PREFIX
 echo "Set CUDA_HOME to ${CUDA_HOME}!"
 
 # Step 6: Install PyTorch with the matching CUDA version
-echo "Installing PyTorch..."
+print_decorated_message "Installing PyTorch..."
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu117
 
 # Step 7: Install other required packages from requirements.txt
-echo "Installing packages from requirements.txt..."
+print_decorated_message "Installing packages from requirements.txt"
 pip install -r requirements.txt
 
-echo "Environment setup is complete!"
+print_decorated_message "Environment setup is complete!"
